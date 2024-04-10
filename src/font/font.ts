65ba7478061fc4004d874bfff54f9e86cb57a851
gustavo <chimenes61@gmail.com>
2024-04-09 21:48:50 -0300

    font

---
+++ src/font/font.ts
@@ -0,0 +1,13 @@
+import { Itim, Nunito } from "next/font/google";
+
+export const ItimFont = Itim({
+  weight: ['400'],
+  style: ['normal'],
+  subsets: ['latin'],
+})
+
+export const NunitoFont = Nunito({
+  weight: ['400', '600', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+})
