81fb5d3e2019b0efddea21e2c39b0873082910ca
gustavo <chimenes61@gmail.com>
2024-04-11 16:08:34 -0300

    SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
-import type { Metadata } from "next";
-import { ItimFont } from "@/font/font";
-import "@/style/globals.css";
+import type { Metadata } from "next"
+import { ItimFont } from "@/font/font"
+import "@/style/globals.css"
 
 export const metadata: Metadata = {
   title: "Protetoras em Ação",
+  description: "Protetoras em Ação é um projeto para ajudar animais",
 };
 
 export default function RootLayout({
