6606fdf5a7c30eff305b03dc3af0b0734f9f9916
gustavo <chimenes61@gmail.com>
2024-04-11 16:07:38 -0300

    adicionar card

--- src/component/carrousel/carrousel.tsx
+++ src/component/carrousel/carrousel.tsx
@@ -1,9 +1,31 @@
+'use client'
+import React from 'react'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 import style from './carrousel.module.css'
 
 export default function Carrousel() {
-  return (
-    <div className={style.carrousel_wrapper}>
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 8000,
+  };
 
+  return (
+    <div className={style.wrapper}>
+      <Slider {...settings}>
+        <a>
+          <img src='./banner/banner_1.png' className={style.banner_image}/>
+        </a>
+        <a>
+          <img src='./banner/banner_2.png' className={style.banner_image}/>
+        </a>         
+      </Slider>
     </div>
   )
 } 
